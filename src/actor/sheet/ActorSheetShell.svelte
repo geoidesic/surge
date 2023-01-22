237daf71f2aa2f71e67173e11232ee8d862c7e98
Noel da Costa <noeldacosta@gmail.com>
2023-01-22 15:11:02 +0000

    removed unused code, tidy up.

--- src/actor/sheet/ActorSheetShell.svelte
+++ src/actor/sheet/ActorSheetShell.svelte
@@ -2,11 +2,8 @@
 
 <script>
   import { ApplicationShell } from "@typhonjs-fvtt/runtime/svelte/component/core";
-  import { debounce } from "@typhonjs-fvtt/runtime/svelte/util";
-  import { getContext, setContext } from "svelte";
+  import { setContext } from "svelte";
   import DocInput from "~/components/DocInput.svelte";
-
-  export let attr = void 0;
 
   export let elementRoot;
   export let storeDoc;
@@ -14,21 +11,9 @@
 
   setContext("#doc", storeDoc);
 
-  const updateDebounce = debounce(update, 500);
-  const { application } = getContext("external");
-  let data;
-
-  $: data = $storeDoc ? $storeDoc?.[attr] : "";
-
-  function update(event) {
-    if ($storeDoc) {
-      $storeDoc.update({ [attr]: event.target.value });
-    }
-  }
   console.log(game);
   console.log(storeDoc);
   console.log(document);
-  console.log(application);
 </script>
 
 <ApplicationShell bind:elementRoot>
--- src/components/DocInput.svelte
+++ src/components/DocInput.svelte
@@ -1,12 +1,10 @@
 <script>
   import { getContext } from "svelte";
-
   import { debounce } from "@typhonjs-fvtt/runtime/svelte/util";
 
   export let attr = void 0;
 
   const doc = getContext("#doc");
-
   const updateDebounce = debounce(update, 500);
 
   let data;
