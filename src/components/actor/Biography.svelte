e6e1b90351598637d79938145908e157a178be21
Noel da Costa <noeldacosta@gmail.com>
2023-02-01 15:24:49 +0000

    - better layout for journal and bio

--- src/components/actor/Biography.svelte
+++ src/components/actor/Biography.svelte
@@ -8,45 +8,56 @@
 
 <template lang="pug">
   ScrollingContainer
-    h1 Appearance
-    .flexrow
-      DocInput(label="gender" attr="system.gender" placeholder="?")
-      DocInput(label="hair" attr="system.hair" placeholder="?")
-      DocInput(label="eyes" attr="system.eyes" placeholder="?")
-      DocInput(label="skin" attr="system.skin" placeholder="?")
-    article
-      h2 physical description
-      ProseMirror( attr="system.appearance" )
-    h1 Personality
-    article
-      h2 Gemeral
-      ProseMirror( attr="system.personality" )
-    article
-      h2 Ideals
-      ProseMirror( attr="system.ideals")
-    article
-      h2 flaws
-      ProseMirror( attr="system.flaws" )
-    article
-      h2 bonds
-      ProseMirror( attr="system.bonds" )
-    h1 Backstory
-    article
-      h2 lore
-      ProseMirror( attr="system.lore" )
-    article
-      h2 contacts
-      ProseMirror( attr="system.contacts" )
-    article
-      h2 factions
-      ProseMirror( attr="system.factions" )
-    article
-      h2 family
-      ProseMirror( attr="system.family" )
-    article
-      h2 zodiac
-      ProseMirror( attr="system.zodiac" )
+    .flexcol.pa-sm
+      h1 Appearance
+      .flexrow
+        DocInput(label="gender" attr="system.gender" placeholder="?")
+        DocInput(label="hair" attr="system.hair" placeholder="?")
+        DocInput(label="eyes" attr="system.eyes" placeholder="?")
+        DocInput(label="skin" attr="system.skin" placeholder="?")
+      .flexcol
+        article
+          h2 physical description
+          ProseMirror( attr="system.appearance" )
+      .flexrow
+        .flexcol
+          h1.flex0 Personality
+          article
+            h2 Gemeral
+            ProseMirror( attr="system.personality" )
+          article
+            h2 Ideals
+            ProseMirror( attr="system.ideals")
+          article
+            h2 flaws
+            ProseMirror( attr="system.flaws" )
+          article
+            h2 bonds
+            ProseMirror( attr="system.bonds" )
+        .flexcol
+          h1.flex0 Backstory
+          article
+            h2 lore
+            ProseMirror( attr="system.lore" )
+          article
+            h2 contacts
+            ProseMirror( attr="system.contacts" )
+          article
+            h2 factions
+            ProseMirror( attr="system.factions" )
+          article
+            h2 family
+            ProseMirror( attr="system.family" )
+          article
+            h2 zodiac
+            ProseMirror( attr="system.zodiac" )
 </template>
 
 <style lang="scss" scoped>
+  .flexrow {
+    gap: 10px;
+  }
+  .flexcol {
+    gap: 10px;
+  }
 </style>
--- src/components/actor/Journal.svelte
+++ src/components/actor/Journal.svelte
@@ -7,19 +7,26 @@
 
 <template lang="pug">
   ScrollingContainer
-    article
-      h2 journal
-      ProseMirror( attr="system.journal")
-    article
-      h2 contacts
-      ProseMirror( attr="system.contacts")
-    article
-      h2 places
-      ProseMirror( attr="system.places")
-    article
-      h2 quests
-      ProseMirror( attr="system.quests")
+    .flexrow.pa-sm
+      .flexcol
+        article
+          h2 journal
+          ProseMirror( attr="system.journal")
+      .flexcol
+        article
+          h2 contacts
+          ProseMirror( attr="system.contacts")
+        article
+          h2 places
+          ProseMirror( attr="system.places")
+        article
+          h2 quests
+          ProseMirror( attr="system.quests")
 </template>
 
 <style lang="scss" scoped>
+  .flexrow,
+  .flexcol {
+    gap: 10px;
+  }
 </style>
