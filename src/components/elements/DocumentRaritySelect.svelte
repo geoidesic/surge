58de8f92e1eec7dccaf1e9f4439514ab1db28572
Noel da Costa <noeldacosta@gmail.com>
2023-02-11 00:34:31 +0000

    #7 - added example of localisation and dynamic css var usage for RaritySelect

---
+++ src/components/elements/DocumentRaritySelect.svelte
@@ -0,0 +1,24 @@
+<script>
+  import RaritySelect from "~/helpers/svelte-components/select/RaritySelect.svelte";
+  import { getContext } from "svelte";
+
+  // The value of the input
+  export let value;
+
+  export let disabled = false;
+
+  // Document reference
+  const document = getContext("#doc");
+</script>
+
+<RaritySelect
+  bind:value
+  disabled={disabled || !$document.isOwner}
+  on:change
+  on:change={async () => {
+    $document.update({
+      system: $document.system,
+      flags: $document.flags,
+    });
+  }}
+/>
