aa4e98ce99465d319d0d67adec1b6a369e5a15e0
Noel da Costa <noeldacosta@gmail.com>
2023-02-22 12:46:11 +0000

    #42 - WIP more spell fields (but some glitches)

--- src/components/item/type/spell/Details.svelte
+++ src/components/item/type/spell/Details.svelte
@@ -4,76 +4,179 @@
   import DocInput from "~/components/item/ItemInput.svelte";
   import { TJSSelect } from "@typhonjs-fvtt/svelte-standard/component";
   import { getContext } from "svelte";
-  import { AOEshapes, timeUnits, effectType } from "~/helpers/Constants.js";
+  import { AOEshapes, timeUnits, effectType, targets } from "~/helpers/Constants.js";
 
   const doc = getContext("#doc");
   $: parentIsActor = $doc.parent?.constructor?.name == "Actor" ? true : false;
+  $: hasReqs = $doc.system.hasReqs;
 </script>
 
 <template lang="pug">
   ScrollingContainer  
     .px-xs
       .flexcol
-        .block
-          h2.mt-none Effects
+        .envelope
+          .flexrow
+
+            .form-group.stacked
+              h2 Spell Components
+              label.checkbox
+                DocumentCheckboxInput(name='system.components.vocal' bind:value="{$doc.system.componentsM.vocal}") 
+                | Verbal
+
+              label.checkbox
+                DocumentCheckboxInput(name='system.components.somatic' bind:value="{$doc.system.componentsM.somatic}") 
+                | Somatic
+
+              label.checkbox
+                DocumentCheckboxInput(name='system.components.material' bind:value="{$doc.system.componentsM.material}") 
+                | Material
+
+              label.checkbox
+                DocumentCheckboxInput(name='system.components.concentration' bind:value="{$doc.system.componentsM.concentration}") 
+                | Concentration
+
+              label.checkbox
+                DocumentCheckboxInput(name='system.components.ritual' bind:value="{$doc.system.componentsM.ritual}") 
+                | Ritual
+
+              label.checkbox
+                DocumentCheckboxInput(name='system.components.rune' bind:value="{$doc.system.componentsM.rune}") 
+                | Rune
+
+         
+          .form-group.stacked
+            label.left Spellcasting Materials
+            label 
+              DocInput(style="width: 100%" attr="system.Mcomponents" placeholder="@todo: link this to a compendium and make it a select. Add `consumed` checkbox. Make it auto-consume resource if present. And it should not allow casting unless material is present. ")
+        .envelope
+          h2.mt-none Detail
           .flexrow
             .labels.flexcol.left
               div
-                label AF (Action Factor)
+                label XP Cost (for level 0)
               div
-                label Effect Type
-              div
-                label Effect Status
-            .values.flexcol.left
-              div
-                input
-              div
-                TJSSelect(options="{effectType}" bind:value="{$doc.system.durationUnits}")
-              div
-                select
+                label Code
+              +if("parentIsActor")
+                label XP
+              +if("parentIsActor")
+                label Level
+              div 
+                label AP cost
+              
+
+            .values.flexcol.left.form-group-stacked
+                div
+                  DocInput(attr="system.xpOffset" inputType="number")
+                div
+                  DocInput(attr="system.code")
+                +if("parentIsActor")
+                  div
+                    DocInput(attr="system.xpAssigned")
+                +if("parentIsActor")
+                  div
+                    DocInput(attr="system.level")
+                div
+                  DocInput(attr="system.APcost")
+                
+
       .flexcol
-        .block
-          h2.mt-none Duration
-          .flexrow
-            .labels.flexcol.left
-              div
-                label Duration Units
-              div
-                label Duration
-              div
-                label Mods
-            .values.flexcol.left
-              div
-                TJSSelect(options="{timeUnits}" bind:value="{$doc.system.durationUnits}")
-              div
-                DocInput(attr="system.duration")
-              div
-                select
-      .flexcol
-        .block
-          h2.mt-none Area
-          .flexrow
+        .envelope
+          h2.mt-none Casting
           
-            .labels.flexcol.left
-              div
-                label Area of Effect?
-              +if("$doc.system.hasAOE")
+          .flexrow.left.mt-xs
+            label.flex1 Action Point Cost (AP)
+            DocInput(attr="system.APcost")
+
+          .flexrow.left.mt-xs
+            label.flex1 Duration
+            .form-fields.flex3.flexrow
+              input.flex0(type='number' step='any' name='system.target.value' bind:value="{$doc.system.duration.value}" placeholder='—')
+              select(name='system.duration.units' data-tooltip='SURGE.DurationUnits' aria-describedby='tooltip' bind:value="{$doc.system.target.units}")
+                option(value)
+                option(value='inst') Instantaneous
+                option(value='turn') Turns
+                option(value='round') Rounds
+                option(value='minute' selected) Minutes
+                option(value='hour') Hours
+                option(value='day') Days
+                option(value='month') Months
+                option(value='year') Years
+                option(value='perm') Permanent
+                option(value='spec') Special
+             
+          .flexrow.left.mt-xs
+            label.flex1 Target
+            .form-fields.flex3.flexrow
+              input.flex0(type='number' step='any' name='system.target.value'  placeholder='—' bind:value="{$doc.system.target.value}")
+              select(name='system.target.units' data-tooltip='SURGE.TargetUnits' aria-describedby='tooltip' bind:value="{$doc.system.target.units}")
+                option(value selected)
+                option(value='ft') Feet
+                option(value='mi') Miles
+                option(value='m') Meters
+                option(value='km') Kilometers
+              select(name='system.target.type' data-tooltip='SURGE.TargetType' aria-describedby='tooltip' bind:value="{$doc.system.target.type}")
+                option(value) None
+                optgroup(label='Individual')
+                  option(value='self') Self
+                  option(value='ally') Ally
+                  option(value='enemy') Enemy
+                  option(value='creature' selected) Creature
+                  option(value='object') Object
+                  option(value='space') Space
+                optgroup(label='Area')
+                  option(value='cone') Cone
+                  option(value='cube') Cube
+                  option(value='cylinder') Cylinder
+                  option(value='line') Line
+                  option(value='radius') Radius
+                  option(value='sphere') Sphere
+                  option(value='square') Square
+                  option(value='wall') Wall
+          .flexrow.left.mt-xs
+            label.flex1 Range
+            .form-fields.flex3.flexrow
+              input(type="number" step="any" name="system.range.value" placeholder="Normal" data-tooltip="DND5E.RangeNormal" aria-describedby="tooltip" bind:value="{$doc.system.range.value}")
+              span.flex0 /
+              input(type='number' step='any' name='system.range.long' value='0' placeholder='Long' data-tooltip='DND5E.RangeLong' aria-describedby='tooltip')
+              select(name='system.range.units' data-tooltip='DND5E.RangeUnits' aria-describedby='tooltip' bind:value="{$doc.system.range.units}")
+                option(value) None
+                option(value='ft' selected) Feet
+                option(value='mi') Miles
+                option(value='m') Meters
+                option(value='km') Kilometers
+                option(value='self') Self
+                option(value='touch') Touch
+                option(value='spec') Special
+                option(value='any') Any
+
+      .px-xs
+        .flexcol
+          .envelope
+            h2.mt-none Effects
+            .flexrow
+              .labels.flexcol.left
                 div
-                  label AOE shape
-              +if("$doc.system.hasAOE")
+                  label AF (Action Factor)
                 div
-                  label AOE scale
-            .values.flexcol.left
-              dib
-                DocumentCheckboxInput.cheeky(bind:value="{$doc.system.hasAOE}")
-              +if("$doc.system.hasAOE")
+                  label Effect Type
                 div
-                  TJSSelect(options="{AOEshapes}" bind:value="{$doc.system.AOEshape}")
-              +if("$doc.system.hasAOE")
+                  label Effect Status
+              .values.flexcol.left
                 div
-                  input
+                  input(type="number bind:value")
+                div
+                  TJSSelect(options="{effectType}" bind:value="{$doc.system.durationUnits}")
+                div
+                  select
+              
+            .form-group.stacked
+              label.left Chat Flavour
+              label 
+                DocInput(style="width: 100%" attr="system.chatFlavour")
+     
       .flexcol
-        .block
+        .envelope
           h2.mt-none Prerequisites
           .flexrow
             .labels.flexcol.left
@@ -81,26 +184,18 @@
                 label Prerequisites?
               +if("$doc.system.hasReqs")
                 div
-                  label Prerequisite Traits
+              div
+                label Prerequisite Type
               +if("$doc.system.hasReqs")
-                div
-                  label Prerequisite Type
+                label Prerequisite Traits
             .values.flexcol.left
               div
                 DocumentCheckboxInput(class="cheeky" bind:value='{$doc.system.hasReqs}')
-              +if("$doc.system.hasReqs")
-                div
-                  select
-              +if("$doc.system.hasReqs")
-                div
-                  select
+              +if("hasReqs")
+                div yo
+              +if("hasReqs")
+                div no
         
-        
-        
-        
-
-
-
 
 </template>
 
@@ -114,9 +209,79 @@
       vertical-align: -webkit-baseline-middle;
     }
   }
-
+  input {
+    margin: 0;
+  }
+  .flexrow,
+  .flexcol {
+    gap: 2px;
+  }
   //- example of child selector
   .block :global(.cheeky) {
     margin: 0 0 0 -2px;
+  }
+  .form-group {
+    clear: both;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: 3px 0;
+  }
+
+  .form-group.stacked label.checkbox,
+  .form-group.stacked label.radio {
+    flex: auto;
+    text-align: left;
+  }
+  .form-group-stacked > *,
+  .form-group.stacked > * {
+    flex: 100%;
+  }
+  .form-group .form-fields {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+  }
+  .envelope {
+    margin: 0.4em 0;
+    padding: 1em;
+    border: 0.4em solid transparent;
+    background: linear-gradient(white, white) padding-box,
+      repeating-linear-gradient(-45deg, #6b1818 0, #112768 15%, #848a0e 0, #4e0a30 20%) 0 / 4em 4em;
+    animation: shimmer 4s ease-in-out infinite;
+  }
+
+  @keyframes shimmer {
+    0% {
+      background-position: 0 0;
+    }
+    50% {
+      background-position: 0.5em 1em;
+    }
+    100% {
+      background-position: 0 0;
+    }
+  }
+
+  $lightblue: #7eb4e2;
+  $darkblue: #32557f;
+
+  .serrated {
+    padding: 10px 10px;
+    border: 10px solid transparent;
+    border-width: 10px 0;
+    background-color: $lightblue;
+    background-color: hsla(0, 0%, 0%, 0);
+    background-image: linear-gradient($lightblue, $darkblue),
+      linear-gradient(to bottom right, transparent 50.5%, $lightblue 50.5%),
+      linear-gradient(to bottom left, transparent 50.5%, $lightblue 50.5%),
+      linear-gradient(to top right, transparent 50.5%, $darkblue 50.5%),
+      linear-gradient(to top left, transparent 50.5%, $darkblue 50.5%);
+    background-repeat: repeat, repeat-x, repeat-x, repeat-x, repeat-x;
+    background-position: 0 0, 10px 0, 10px 0, 10px 100%, 10px 100%;
+    background-size: auto auto, 20px 20px, 20px 20px, 20px 20px, 20px 20px;
+    background-clip: padding-box, border-box, border-box, border-box, border-box;
+    background-origin: padding-box, border-box, border-box, border-box, border-box;
   }
 </style>
