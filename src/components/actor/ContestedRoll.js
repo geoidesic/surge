44f7ca7fdd5444569e1f5ea35c923f9fe322e279
Noel da Costa <noeldacosta@gmail.com>
2023-02-25 18:38:58 +0000

    #57 - added ContestedRoll class stub

---
+++ src/components/actor/ContestedRoll.js
@@ -0,0 +1,19 @@
+import { isNumber } from "../../helpers/Utility";
+export default class ContestedRoll {
+  #params = void 0;
+
+  constructor(params) {
+    this.#params = params;
+  }
+
+  weapon(item, actor) {
+    //- get array of targets
+    //- get map of target intents
+    //- get contest codes from intents
+    //- offer any dialog options to targets
+    //- once all options are gathered, roll for both parties
+    //- output to chat
+    //- apply effects to target
+  }
+
+}
