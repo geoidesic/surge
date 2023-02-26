89e2e927f1c824ff6a68fb6f3f96b6edd7fea6a5
Noel da Costa <noeldacosta@gmail.com>
2023-02-26 01:44:50 +0000

    #57 - added intent selector to combat tracker

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ import "./styles/Global.scss";
 import SurgeActorSheet from "./sheets/actor/ActorSheet";
 import SurgeItemSheet from "./sheets/item/ItemSheet";
 import SurgeRoll from './view/chatmessage/SurgeRoll.svelte';
+import { intents } from "./helpers/Constants.js"
+import CombatTracker from './extensions/combat-tracker.js'
 
 
 //- set to true to see which hooks get called
 CONFIG.debug.hooks = true;
+CONFIG.Combat.entityClass = CombatTracker;
 
 Hooks.once("init", async () => {
   console.log("SURGE | Starting Surge VTTRPG System");
@@ -47,7 +50,7 @@ Hooks.on('renderChatMessage', (message, html) => {
 /**
  * Used by chat message demo to clean up / destroy the mounted Svelte component to the message instance when the chat
  * message is deleted.
- */
+*/
 Hooks.on('preDeleteChatMessage', (message) => {
   // Find associated flag data scoped to your module ID. This is the easiest way to determine that this message is
   // associated with your module and has a Svelte component attached to the message content.
@@ -68,4 +71,35 @@ Hooks.on('preUpdateActiveEffect', (effect) => {
 Hooks.on('closeActiveEffectConfig', (effect) => {
   console.log("preUpdateActiveEffect")
   console.log(effect)
+})
+Hooks.on('renderCombatTracker', (app, html, options) => {
+  const $lis = $('li.combatant');
+
+  $lis.each((i, li) => {
+    // create a new child element
+    const combatantId = $(li).attr('data-combatant-id');
+    const combatant = game.combat.combatants.get(combatantId)
+    const Actor = game.actors.get(combatant._source.actorId)
+
+    const $select = $(`<select style="color: white; max-height: 2rem; margin-top: 0.5rem" >`);
+    intents.forEach((option) => {
+      const $option = $(`<option value="${option.value}">`);
+      $option.text(option.label);
+      $select.append($option);
+    });
+    $select.val(Actor.system.intent);
+    $select.on('change', function (event) {
+      const value = $(this).val();
+      Actor.update({ ['system.intent']: value })
+    })
+    // get the children of the li element
+    const $children = $(li).children();
+
+    // insert the new element as the penultimate child element
+    if ($children.length > 1) {
+      $select.insertBefore($children[$children.length - 1]);
+    } else {
+      $(li).append($select);
+    }
+  });
 })
